b01767b072f629fc1295226fffc021f4ee05cb62
Tolulope Abiola <anotibills@gmail.com>
2023-09-22 16:00:59 +0100

    Task 0_1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #This code will create file 'school' in /tmp directory
 file {  '/tmp/school':
-  path		=> '/tmp/school',
-  ensure	=> 'file',
-  mode		=> '0774',
-  owner		=> 'www-data',
-  group		=> 'www-data',
-  content	=> 'I love Puppet',
+  ensure  => 'file',
+  path    => '/tmp/school',
+  mode    => '0774',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
