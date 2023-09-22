4b15fb31acd224fb1a298777bc1c2c5ff3c3acfe
Tolulope Abiola <anotibills@gmail.com>
2023-09-22 16:03:47 +0100

    Task 0_2

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 #This code will create file 'school' in /tmp directory
 file {  '/tmp/school':
   ensure  => 'file',
-  path    => '/tmp/school',
   mode    => '0774',
   owner   => 'www-data',
   group   => 'www-data',
