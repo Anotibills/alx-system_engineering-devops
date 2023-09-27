ddc55574f80a118d4590709d364fc03f285a9a2c
Tolulope Abiola <anotibills@gmail.com>
2023-09-26 22:17:31 -0700

    puppet corrected

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Puppet script for ssh configuration
 
-file { "/etc/ssh/ssh_config",
+file { '/etc/ssh/ssh_config':
         ensure  => present,
         content => "
                     #Script for SSH Configuration
