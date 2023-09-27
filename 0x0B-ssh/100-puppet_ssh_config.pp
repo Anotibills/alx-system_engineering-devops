0a244d6a02e621c652298b4d38d45977f0a3c4ca
Tolulope Abiola <anotibills@gmail.com>
2023-09-26 20:40:11 -0700

    Puppet script

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Puppet script for ssh configuration
+
+file { "/etc/ssh/ssh_config",
+        ensure  => present,
+        content => "
+                    #Script for SSH Configuration
+                    Host *
+                         PasswordAuthentication no
+                         IdentityFile ~/.ssh/school
+                   ",
+}
