1e3c874a3e451b2f9cb26e531bbf0533e5e7ff63
Tolulope Abiola <anotibills@gmail.com>
2023-11-08 12:08:29 +0100

    debug

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+#Puppet script that automate fixing of php error in Word press
+
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin:/bin',
+}
+
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+This is another debugging project using the strace
